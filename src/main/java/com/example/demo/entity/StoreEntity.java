a932930d795921078ed09ecaf874c243411e3977
bala-amypo <bala@amypo.in>
2025-12-17 08:24:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/StoreEntity.java
+++ src/main/java/com/example/demo/entity/StoreEntity.java
@@ -4,4 +4,10 @@ public class StoreEntity{
      @id
      @Generatedvalue(strategy=GenerationType.IDENTITY)
      private long id;
+     @Column(unique=true)
+     private String storeName;
+     private String address;
+     private String region;
+     private Boolean active=true;
+     
 }
