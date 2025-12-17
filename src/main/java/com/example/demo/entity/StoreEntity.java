7a06485f0278f6db6cd5c021cda825ee0b3875ce
bala-amypo <bala@amypo.in>
2025-12-17 08:28:40 +0000

    User commit

--- src/main/java/com/example/demo/entity/StoreEntity.java
+++ src/main/java/com/example/demo/entity/StoreEntity.java
@@ -3,11 +3,23 @@
 public class StoreEntity{
      @id
      @Generatedvalue(strategy=GenerationType.IDENTITY)
-     private long id;
+     private Long id;
      @Column(unique=true)
      private String storeName;
      private String address;
      private String region;
      private Boolean active=true;
      
+
+     public void setId(Long id){
+        this.id=id;
+     }
+     public Long getId(){
+        return id;
+     }
+
+     public void setStoreName(String storename){
+        
+     }
+
 }
