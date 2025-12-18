13bb7561db210c5e8c85305b767e6f298dfb58e9
bala-amypo <bala@amypo.in>
2025-12-18 08:58:36 +0000

    User commit

--- src/main/java/com/example/demo/service/implement/StoreImplementation.java
+++ src/main/java/com/example/demo/service/implement/StoreImplementation.java
@@ -1,20 +1,21 @@
 package com.example.demo.service.implement;
 import com.example.demo.service.StoreService;
 import com.example.demo.entity.Store;
-import com.example.
+import com.example.demo.repository.StoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 public class StoreImplementation implements StoreService{
     @Autowired
     StoreRepository obj;
     Store createStore(Store store){
-          obj.save(store);
+          return obj.save(store);
     }
 
     Store getStoreId(Long id){
 
-        obj.findById(id);
+        return obj.findById(id);
     }
 
     Store getAllStores(){
-        obj.findAll();
+        return obj.findAll();
     }
 }
