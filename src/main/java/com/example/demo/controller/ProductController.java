059027aa7b32ad6175f686b50896d771a79eb364
bala-amypo <bala@amypo.in>
2025-12-18 16:34:46 +0000

    User commit

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,8 +1,18 @@
 package com.example.demo.controller;
 import org.springframework.web.bind.annotation.Autowired;
-import org.springframework.web.bind.annotation.;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.example.demo.service.ProductService;
 
-
+@RestController
+@RequestMapping("/api/products")
 public class ProductController{
     @Autowired
+    ProductService obj;
+    
+
 }
