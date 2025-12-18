11d2328d9abadae1b7961b96c1a9391ead595c22
bala-amypo <bala@amypo.in>
2025-12-18 08:35:26 +0000

    User commit

--- src/main/java/com/example/demo/controller/StoreController.java
+++ src/main/java/com/example/demo/controller/StoreController.java
@@ -1,5 +1,9 @@
 package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.demo.entity
 
 public class StoreController{
     @Autowired
