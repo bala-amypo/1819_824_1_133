abdca1d0ed7d814abbcf61eaf9814d8f56a47580
bala-amypo <bala@amypo.in>
2025-12-18 07:58:36 +0000

    User commit

--- src/main/java/com/example/demo/service/StoreService.java
+++ src/main/java/com/example/demo/service/StoreService.java
@@ -1,6 +1,8 @@
-import com.example.demo.entity.
-
+package com.example.demo.service;
+import com.example.demo.entity.Store;
+import org.springframework.stereotype.Service;
+@Service
 public interface StoreService{
     Store createStore(Store store);
-    Long getStoreId(Long id);
+    Store getStoreId(Long id);
 }
